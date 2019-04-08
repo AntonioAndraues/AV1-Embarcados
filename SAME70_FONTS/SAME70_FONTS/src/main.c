3259afe7cca16f677460ca0d8c41e9da7b6d6de4
AntonioAndraues <juniorbrgamers@gmail.com>
2019-04-08 15:43:17 -0300

    debuug

--- SAME70_FONTS/SAME70_FONTS/src/main.c
+++ SAME70_FONTS/SAME70_FONTS/src/main.c
@@ -5,6 +5,8 @@
  *  Author: eduardo
  */ 
 
+
+
 #include <asf.h>
 #include "tfont.h"
 #include "sourcecodepro_28.h"
@@ -13,6 +15,66 @@
 
 
 struct ili9488_opt_t g_ili9488_display_opt;
+/************************************************************************/
+/* variaveis globais                                                    */
+
+int quantidades_de_rotacoes=1;
+char buffer[32];
+volatile Bool f_rtt_alarme = false;
+/************************************************************************/
+/* prototypes                                                           */
+
+void pin_toggle(Pio *pio, uint32_t mask);
+void io_init(void);
+static void RTT_init(uint16_t pllPreScale, uint32_t IrqNPulses);
+/************************************************************************/
+/* defines                                                              */
+/************************************************************************/
+// Bot?o
+#define BUT1_PIO      PIOB
+#define BUT1_PIO_ID   ID_PIOB
+#define BUT1_IDX  1
+#define BUT1_IDX_MASK (1 << BUT1_IDX)
+
+#define LED_PIO       PIOC
+#define LED_PIO_ID    ID_PIOC
+#define LED_IDX       8u
+#define LED_IDX_MASK  (1u << LED_IDX)
+
+/************************************************************************/
+/* funcoes                                                              */
+/************************************************************************/
+
+void but_callback(void)
+{
+	quantidades_de_rotacoes+=1;
+	sprintf(buffer,"%d",quantidades_de_rotacoes);
+}
+void io_init(void)
+{
+	// Inicializa clock do perif?rico PIO responsavel pelo botao
+	pmc_enable_periph_clk(BUT1_PIO_ID);
+	pmc_enable_periph_clk(LED_PIO_ID);
+	pio_configure(LED_PIO, PIO_OUTPUT_0, LED_IDX_MASK, PIO_DEFAULT);
+	// Configura PIO para lidar com o pino do bot?o como entrada
+	// com pull-up
+	pio_configure(BUT1_PIO, PIO_INPUT, BUT1_IDX_MASK, PIO_PULLUP);
+	pio_handler_set(BUT1_PIO,
+	BUT1_PIO_ID,
+	BUT1_IDX_MASK,
+	PIO_IT_FALL_EDGE,
+	but_callback);
+		// Ativa interrup??o
+	pio_enable_interrupt(BUT1_PIO, BUT1_IDX_MASK);
+	NVIC_EnableIRQ(BUT1_PIO_ID);
+	NVIC_SetPriority(BUT1_PIO_ID, 3); // Prioridade 3
+}
+void pin_toggle(Pio *pio, uint32_t mask){
+	if(pio_get_output_data_status(pio, mask))
+	pio_clear(pio, mask);
+	else
+	pio_set(pio,mask);
+}
 
 void configure_lcd(void){
 	/* Initialize display parameter */
@@ -41,17 +103,64 @@ void font_draw_text(tFont *font, const char *text, int x, int y, int spacing) {
 		p++;
 	}	
 }
+static float get_time_rtt(){
+	uint ul_previous_time = rtt_read_timer_value(RTT);
+}
 
+static void RTT_init(uint16_t pllPreScale, uint32_t IrqNPulses)
+{
+	uint32_t ul_previous_time;
+
+	/* Configure RTT for a 1 second tick interrupt */
+	rtt_sel_source(RTT, false);
+	rtt_init(RTT, pllPreScale);
+	
+	ul_previous_time = rtt_read_timer_value(RTT);
+	while (ul_previous_time == rtt_read_timer_value(RTT));
+	
+	rtt_write_alarm_time(RTT, IrqNPulses+ul_previous_time);
+
+	/* Enable RTT interrupt */
+	NVIC_DisableIRQ(RTT_IRQn);
+	NVIC_ClearPendingIRQ(RTT_IRQn);
+	NVIC_SetPriority(RTT_IRQn, 0);
+	NVIC_EnableIRQ(RTT_IRQn);
+	rtt_enable_interrupt(RTT, RTT_MR_ALMIEN);
+}
+/************************************************************************/
+/* interrupcoes                                                         */
+/************************************************************************/
+
+void RTT_Handler(void)
+{
+	uint32_t ul_status;
+
+	/* Get RTT status */
+	ul_status = rtt_get_status(RTT);
+
+	/* IRQ due to Time has changed */
+	if ((ul_status & RTT_SR_RTTINC) == RTT_SR_RTTINC) {  }
+
+	/* IRQ due to Alarm */
+	if ((ul_status & RTT_SR_ALMS) == RTT_SR_ALMS) {
+		pin_toggle(LED_PIO, LED_IDX_MASK);    // BLINK Led
+		f_rtt_alarme = true;                  // flag RTT alarme
+	}
+}
+
+	/* Initialize TC */
 
 int main(void) {
 	board_init();
 	sysclk_init();	
 	configure_lcd();
-	
+	sprintf(buffer,"%d",quantidades_de_rotacoes);
 	font_draw_text(&sourcecodepro_28, "OIMUNDO", 50, 50, 1);
 	font_draw_text(&calibri_36, "Oi Mundo! #$!@", 50, 100, 1);
-	font_draw_text(&arial_72, "102456", 50, 200, 2);
+	font_draw_text(&arial_72, buffer, 50, 200, 2);
+
 	while(1) {
+		pmc_sleep(SAM_PM_SMODE_SLEEP_WFI);
 		
 	}
 }
